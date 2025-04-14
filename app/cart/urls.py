cf3828a9e20d88f54bbb8f30720135cb7349200e
Jugadveer Singh <mail@example.com>
2025-04-15 01:15:50 +0530

    first

---
+++ app/cart/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+from .views import payment_view
+
+urlpatterns = [
+    path('', views.cart_summary, name="cart_summary"),
+    path('add/', views.cart_add, name="cart_add"),
+    path('delete/', views.cart_delete, name="cart_delete"),
+    path('update/', views.cart_update, name="cart_update"),
+    path('checkout/', views.checkout, name = "checkout"),
+    path('process_order', views.process_order, name="process_order"),
+    path('shipped_dash', views.shipped_dash, name="shipped_dash"),
+    path('not_shipped_dash', views.not_shipped_dash, name="not_shipped_dash"),
+    path('orders/<int:pk>', views.orders, name='orders'),
+    path('cart/payment/', views.payment_view, name='payment')
+
+]
